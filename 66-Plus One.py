984dd9ccbbc347cc8b7a2d45dd29939961c3999f
Gabrenk <128539490+Gabrenk@users.noreply.github.com>
2023-05-05 19:19:21 -0300

    Update 66-Plus One.py

--- 66-Plus One.py
+++ 66-Plus One.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        digit[-1]
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        else:
+            digits.reverse()
+            carry = 1
+            for i in range(len(digits)):
+                digits[i] += carry
+                if digits[i] == 10:
+                    digits[i] = 0
+                else:
+                    carry = 0
+                if digits[i] is digits[-1] and carry==1:
+                    digits.append(1)
+            digits.reverse()
+        
+        return digits

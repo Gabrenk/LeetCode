aa103b208aca9915f04debaea10e7f45df526e18
Gabrenk <128539490+Gabrenk@users.noreply.github.com>
2023-05-05 19:30:25 -0300

    Update 66-Plus One.py

--- 66-Plus One.py
+++ 66-Plus One.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
+        #Since any number, except 9, plus one is always equal to 
+        # a single digit number, check it
         if digits[-1] < 9:
             digits[-1] += 1
             return digits
+        #If the last number is 9 we'll begin a loop that will add the
+        #carry to the last digit,then the to the last -1 and so forth. 
+        #Remembering that the carry will be zeroed if digits[i]+carry =10
+        #And then, if our carry remains equal to 1, create a new house for it
         else:
             digits.reverse()
             carry = 1
